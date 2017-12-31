1608ef5ca83c26f0382ba263609afdcc346d5eda
schreyerpeter <pschreyer12321@aol.com>
2017-12-30 20:22:25 -0800

    #3 Connected MongoDB and built a GET and POST API for favorites.

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const request = require('request');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 const app = express();
+const url = 'mongodb://localhost:27017/buoybuddy' || process.env.MONGOLAB_URI;
 
-app.use(express.static('public'))
 app.use(bodyParser());
+app.use(express.static('public'));
 app.use((req,res,next) => {
     res.header('Access-Control-Allow-Origin', '*'),
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Credentials', true);
     next();
 })
 
@@ -22,6 +26,37 @@ app.get('/buoys', (req,res) => {
         else res.sendStatus(400);
     });
 });
-
+app.get('/favorites', (req,res) => {
+    MongoClient.connect(url, function(err, db){
+        assert.equal(null,err);
+        const buoyBuddyDb = db.db('buoybuddy');
+        const favorites = buoyBuddyDb.collection('favorites');
+        favorites
+        .find({})
+        .toArray(function(error, documents) {
+            res.send(documents);
+            db.close();
+        })
+    })
+})
+app.post('/favorites/:id', (req,res) => {
+    MongoClient.connect(url, function(err, db){
+        assert.equal(null,err);
+        const buoyBuddyDb = db.db('buoybuddy');
+        const favorites = buoyBuddyDb.collection('favorites');
+        favorites
+        .insertOne({
+          id: req.params.id
+        }, function(error, results){
+          if (error){
+            db.close();
+            res.send("Something went wrong");
+          } else {
+            db.close();
+            res.send("Successful post");
+          }
+        })
+    })
+});
 
 app.listen(process.env.PORT || 3001);
